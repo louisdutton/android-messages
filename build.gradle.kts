63a866a707a80b50f5b7e776f3382708e5eaa98a
Louis Dutton <louis@dutton.digital>
2025-05-24 18:56:46 +0100

    fix dev env

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 buildscript { repositories { google() } }
 
 plugins {
-	kotlin("android") version "1.9.24" apply false
-  id("com.android.application") version "8.5.0" apply false
-  id("com.android.library") version "8.5.0" apply false
+  kotlin("android")                   version "1.9.24" apply false
+  id("com.android.application")       version "8.5.0"  apply false
+  id("com.android.library")           version "8.5.0"  apply false
   id("com.github.ben-manes.versions") version "0.51.0" apply false
 }
--- flake.nix
+++ flake.nix
@@ -21,10 +21,15 @@
           };
         };
 
+        platformVersion = "34";
         buildToolsVersion = "34.0.0";
+        systemImageType = "default";
         androidPkgs = pkgs.androidenv.composeAndroidPackages {
           buildToolsVersions = [buildToolsVersion];
-          platformVersions = ["34"];
+          platformVersions = [platformVersion "28"];
+          systemImageTypes = [systemImageType];
+          useGoogleAPIs = false;
+          includeSystemImages = true;
         };
         androidSdk = androidPkgs.androidsdk;
       in
@@ -39,6 +44,17 @@
             ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
             ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk-bundle";
             GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
+          };
+
+          packages.emulator = androidenv.emulateApp {
+            inherit systemImageType;
+            name = "example";
+            abiVersion = "arm64-v8a"; # armeabi-v7a, mips, x86, x86_64
+            platformVersion = "34";
+            # TODO: install app (difficult as the apk isn't tracked in git)
+            # app = ./example.apk;
+            # package = "digital.dutton.exampleapp";
+            # activity = "digital.dutton.exampleapp.App";
           };
         }
     );
